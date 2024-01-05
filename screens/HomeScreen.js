45e4499d84804a3d42f560e98043c8115b218a77
Hartz-byte <harshmail281199@gmail.com>
2024-01-05 12:28:58 +0530

    home start

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,35 @@
-import { SafeAreaView, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, Text, View, Image, TextInput } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { theme } from "../theme";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
-  )
-}
+    <View className="flex-1 relative">
+      {/* status bar and background image */}
+      <StatusBar style="light" />
+      <Image
+        blurRadius={60}
+        source={require("../assets/images/bg.png")}
+        className="absolute h-full w-full"
+      />
 
-export default HomeScreen
+      <SafeAreaView className="flex flex-1">
+        {/* search section */}
+        <View style={{ height: "7%" }} className="mx-4 relative z-50">
+          <View
+            className="flex-row justify-end items-center rounded-full"
+            style={{ backgroundColor: theme.bgWhite(0.2) }}
+          >
+            <TextInput
+              placeholder="Search city"
+              placeholderTextColor={"lightgray"}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default HomeScreen;
